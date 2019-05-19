052bd882c857a0b633de394fc127407a502b305d
subhash9758964966 <subhashsagar98@gmail.com>
2019-05-19 11:27:27 +0530

    first commit

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Menu from './components/Menucomponent';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Menu/>
     </div>
   );
+  }
 }
 
 export default App;
