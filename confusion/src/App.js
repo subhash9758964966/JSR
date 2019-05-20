90fe5d3aa5c4a971f52b424feed95fd6cc2ac80e
subhash9758964966 <subhashsagar98@gmail.com>
2019-05-20 10:35:25 +0530

    second commit

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import Menu from './components/Menucomponent';
+import {DISHES} from './shared/dishes';
 import './App.css';
 
 class App extends Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state={
+        dishes: DISHES
+    };
   }
   render(){
   return (
     <div >
-      <Menu/>
+      <Menu dishes={this.state.dishes}/>
     </div>
   );
   }
