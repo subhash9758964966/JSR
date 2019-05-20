ef1f7f0c88d9a70eef58a81737a43855b9c6f0ce
subhash9758964966 <subhashsagar98@gmail.com>
2019-05-20 11:36:13 +0530

    second exercise

--- confusion/src/components/Menucomponent.js
+++ confusion/src/components/Menucomponent.js
@@ -1,25 +1,46 @@
 import React, {Component} from 'react';
 import {Media} from 'reactstrap';
-import {Card, CardImg, CardImgOverlay, CardText, Cardbody, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 
 class Menu extends Component{
     constructor(props){
         super(props);
         this.state={
-            
+            selectedDish :null
                         
     };
 }
+   onDishSelected(dish){
+                  this.setState({selectedDish :dish});
+   }
+   renderDish(dish){
+       if(dish!=null){
+           return(
+                    <Card>
+                         <CardImg width = "100%" src ={dish.image} alt ={dish.name}/>
+                         <CardBody>
+                             <CardTitle>{dish.name}</CardTitle>
+                             <CardText>{dish.description}</CardText>
+                         </CardBody>
+                    </Card>
+           );
+
+       }
+       else{
+           return(
+           <div></div>
+           );
+       }
+   }
     render(){
         const menu =this.props.dishes.map((dish)=>{
                     return(
                          <div key={dish.id} className=" col-12 col-md-5 m-1">
-                            <Card >
+                            <Card onClick={()=>this.onDishSelected(dish)} >
                                     
                                         <CardImg width = "100%" src ={dish.image} alt ={dish.name}/>
                                             <CardImgOverlay>
                                                 <CardTitle >{dish.name}</CardTitle>
-                                                      <p>{dish.description}</p>
                                            </CardImgOverlay>
                             </Card>
                          </div>
@@ -31,6 +52,10 @@ class Menu extends Component{
                      
                       {menu}
                      
+                   </div>
+                   <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
+                       
                    </div>
                 </div>            
 
